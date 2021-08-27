284dd9db4afda999439bd89c11444cdc8160636b
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2021-08-27 10:53:08 -0700

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category);
+
+Category.hasMany(Product);
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
