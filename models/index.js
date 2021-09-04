6c6e3737316d574211432fb1c6f28901366f2e62
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2021-09-03 19:12:56 -0700

    new models file
    been working on them since group project over

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const Product = require('./product');
+const Category = require('./category');
+const Tag = require('./tag');
+const ProductTag = require('./productTag');
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "cascade"
+})
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+})
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
